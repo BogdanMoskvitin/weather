c4f7b5823795baefa91630d5754f9a000759a5a8
Bogdan Moskvitin <moskvitin.bg@gmail.com>
2021-06-14 13:50:41 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'weather';
+  response: any;
+  location: string;
+  
+  answer: any;
+
+  constructor(private http: HttpClient, private httpp: HttpClient) {}
+
+  search() {
+    this.http.get('http://api.weatherstack.com/current?access_key=098e907f03c8093afa860f752a04ad99&query=' + this.location)
+    .subscribe((response)=>{
+      this.response = response;
+      console.log(this.response);
+    })
+
+    this.httpp.get('https://api.unsplash.com/photos/random?client_id=KfeHbiF-wbdoRKQwOEFc5J3W5cf1O6MkGbhaPuE-Aoc&query=' + this.location + '&orientation=landscape')
+    .subscribe((answer)=>{
+      this.answer = answer;
+      console.log(this.answer);
+    })
+  }
 }
